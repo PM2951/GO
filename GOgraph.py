12b0484c82c4e5b573d802e96037b9aadfeafa47
PM Laboratry <122586986+PM2951@users.noreply.github.com>
2024-12-09 13:19:18 +0900

    Add files via upload

--- GOgraph.py
+++ GOgraph.py
@@ -1,3 +1,4 @@
+
 import requests
 import streamlit as st
 import pandas as pd
@@ -6,66 +7,33 @@ from matplotlib import pyplot as plt
 from io import BytesIO
 import matplotlib.font_manager as fm
 
-FONT_PATH = '/content/GO/font/Arial.ttf'
-fp = fm.FontProperties(fname=FONT_PATH)
+# FONT_PATH = '/content/GO/font/Arial.ttf'
+# fp = fm.FontProperties(fname=FONT_PATH)
 
-font_files = fm.findSystemFonts(fontpaths='/content/GO/font/')
-for font_file in font_files:
-    fm.fontManager.addfont(font_file)
-plt.rcParams['font.family'] = 'Arial'
+# font_files = fm.findSystemFonts(fontpaths='/content/GO/font/')
+# for font_file in font_files:
+#     fm.fontManager.addfont(font_file)
 
-# def term_cov(x):
-#     if 'id' in x:
-#       id = x['id']
-#       label = x['label']
-#       name = f'{label}({id})'
-#     else:
-#       name = x['label']
-#     return name
+with st.spinner('Wait for it...'):
+    plt.rcParams['font.family'] = 'Arial'
 
-# def pantherGO(gene_list):
-#     base_url = "https://pantherdb.org/"
-#     endpoint = "services/oai/pantherdb/enrich/overrep"
-#     params = {
-#         "geneInputList": ','.join(gene_list),  # 遺伝子リスト
-#         "organism": "3702",
-#         "annotDataSet": 'GO:0008150',
-#         "enrichmentTestType": "FISHER",
-#         "correction": "BONFERRONI",
-#     }
-#     response = requests.post(base_url + endpoint, params=params)
-#     result_df = pd.DataFrame(response.json()['results']['result'])
-#     if len(result_df) == 0:
-#         raise ValueError('遺伝子リストが空です！')
-#     try:
-#       result_df_sig = result_df[(result_df['pValue'] < 0.05) & (
-#           result_df['fold_enrichment'] > 0)].copy()
-#       result_df_sig = result_df_sig.sort_values(
-#           by='fold_enrichment', ascending=False)
-#       result_df_sig['term'] = result_df_sig['term'].apply(term_cov).copy()
-#       # reference = response.json()['results']['reference']
-#       # input_list = response.json()['results']['input_list']
-#       result_df_sig = result_df_sig[[
-#           'term', 'number_in_list', 'fold_enrichment', 'pValue']]
-#     # print(result_df_sig)
-#       return result_df_sig
-#     except:
-#         raise ValueError('エラー')
 
 
 def make_graph(data, height, width, count, vmax, vmin, cmap, plotsize, xmin, xmax, n=3):
     fig = plt.figure(figsize=(width, height))
     data['fold_enrichment'] = data['fold_enrichment'].astype(float)
     module_GO_top = data.sort_values(by='fold_enrichment', ascending=False).head(int(count))
-    module_GO_top = module_GO_top.sort_values(
-        by='fold_enrichment', ascending=True)
+    module_GO_top = module_GO_top.sort_values(by='fold_enrichment', ascending=True)
     y = module_GO_top['term']
     x = module_GO_top['fold_enrichment'].astype(float)
     c = module_GO_top['pValue'].astype(float)
     size = module_GO_top['number_in_list'].astype(int)
     
-    plt.scatter(x, y, c=c, s=size*plotsize, linewidths=0.5,
-                edgecolors='black', cmap=cmap,
+    plt.scatter(x, y, c=c, 
+                s=size*plotsize, 
+                linewidths=0.5,
+                edgecolors='black', 
+                cmap=cmap,
                 norm=LogNorm(vmax=vmax, vmin=vmin))
 
     # グラフの設定
@@ -82,7 +50,7 @@ def make_graph(data, height, width, count, vmax, vmin, cmap, plotsize, xmin, xma
         label = round(l)
         plt.scatter([], [], c='k', s=size_i, label=label)
     plt.legend(loc="upper right", bbox_to_anchor=(0.5, -0.15),
-               frameon=False, ncol=6, title='Gene count', prop=fp)
+               frameon=False, ncol=6, title='Gene count')
     plt.tight_layout()
     st.pyplot(fig)
     return fig
@@ -106,7 +74,7 @@ cmap = st.sidebar.selectbox('Color',
 plotsize = st.sidebar.number_input('Plot size', value=1.5)
 n = st.sidebar.number_input('GeneCountの数', value=3)
 xmin = st.sidebar.number_input('x軸の最小値', value=0)
-xmax = st.sidebar.number_input('x軸の最大値', value=50)
+xmax = st.sidebar.number_input('x軸の最大値', value=25)
 fontsize = st.sidebar.number_input('フォントサイズ', value=12)
 caption = st.sidebar.write('='*20)
 
@@ -121,6 +89,7 @@ if len(txt) != 0:
     data = data.reset_index(drop=True)
     data.columns = ['term', 'REFLIST (27430)', "number_in_list",
                     "expected count", "(over/under)", "fold_enrichment", "pValue"]
+    st.info('列名と入力したデータが一致しているか確認してください', icon="ℹ️")
     st.write(data)
     if len(data) > 0:
         # GOグラフ作成
